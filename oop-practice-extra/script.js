d55a7d39f3a719d05e919761b2479c4df784ef32
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-17 13:20:45 +0300

    oop-practice

--- oop-practice-extra/script.js
+++ oop-practice-extra/script.js
@@ -1,4 +1,4 @@
-const DomElement = function(selector, height, width, bg = '#ccc', fontSize = '14px') {
+const DomElement = function (selector, height, width, bg = '#ccc', fontSize = '14px') {
     this.selector = selector;
     this.height = height;
     this.width = width;
@@ -6,9 +6,9 @@ const DomElement = function(selector, height, width, bg = '#ccc', fontSize = '14
     this.fontSize = fontSize;
 }
 
-DomElement.prototype.createElement = function() {
+DomElement.prototype.createElement = function(position) {
     let sel = this.selector.split('')[0];
-    let newSelector = this.selector.split('').splice(1, 6).join('');
+    let newSelector = this.selector.split('').splice(1, 50).join('');
     let div = document.createElement('div');
     if (sel === '.') {
         document.body.appendChild(div);
@@ -17,41 +17,39 @@ DomElement.prototype.createElement = function() {
         document.body.appendChild(div);
         div.setAttribute('id', newSelector);
     };
+    div.style.position = position;
+    console.log(getComputedStyle(div).position);
+    console.log(getComputedStyle(div).left);
+    console.log(getComputedStyle(div).bottom);
+
     div.textContent = 'я элемент';
-    div.style.cssText = `position: absolute; top: 200px; left: 200px;
-    text-align: center; margin: 30px;
-    height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; fontSize: ${this.fontSize}px;`;
+    div.style.cssText = `text-align: center; height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; fontSize: ${this.fontSize}px;`;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const newElem = new DomElement('.square', '100', '100', 'orange', 16);
-    let body = document.body;
-    let elem = document.getElementsByTagName('div');
-    console.log(elem);
-    
-    body.style.position = 'relative';
-    body.addEventListener('keydown', (event) => {
-            console.dir(window.getComputedStyle(elem[0]));
-            console.log(event.key);
-            if (event.key = 'ArrowDown') {
-                elem[0].style.top = '100';
-            }
-            else if (event.key = 'ArrowUp') {
-                elem[0].style.top = '150';
-            }
-            else if (event.key = 'ArrowLeft') {
-                elem[0].style.left = '100';
-            }
-            else if (event.key = 'ArrowRight') {
-                elem[0].style.left = '150';
-            }
-    });
-    document.body.addEventListener('keyup', (event) => {
-        elem[0].style.top = '200';
-        elem[0].style.left = '200';
-    })
-    newElem.createElement();
+const newElem = new DomElement('.square', '100', '100', 'orange', '16');
+document.addEventListener('DOMContentLoaded',() => {
+    newElem.createElement('absolute');
+});
+
+window.addEventListener('keydown', function (event) {
+    let elem = document.querySelector('.square');
+    let computedStyle = getComputedStyle(elem);
+    elem.style.left = '100px';
+    elem.style.bottom = '100px';
+
+    function callback(step, position) {
+        console.log(step);
+        console.log(parseInt(position));
+        console.log((step + parseInt(position) + 'px'));
+        return (step + parseInt(position) + 'px');
+    }
+    if (event.key = 'ArrowDown') {
+        elem.style.bottom = callback(-10, computedStyle.bottom);
+    } else if (event.key = 'ArrowUp') {
+        elem.style.bottom = callback(10, computedStyle.bottom);
+    } else if (event.key = 'ArrowLeft') {
+        elem.style.left = callback(-10, computedStyle.left);
+    } else if (event.key = 'ArrowRight') {
+        elem.style.left = callback(10, computedStyle.left);
+    }
 })
-
-
-
