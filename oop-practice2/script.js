ed039d9771d12c627cc52522f30e9f455fecc676
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-19 20:48:36 +0300

    oop

--- oop-practice2/script.js
+++ oop-practice2/script.js
@@ -1,15 +1,10 @@
 class First {
-    constructor() {}
     hello() {
         console.log('Привет я метод родителя!');
     }
 }
 
 class Seconds extends First {
-    constructor() {
-        super()
-    }
-
     hello() {
         super.hello();
         console.log('А я наследуемый метод!');
