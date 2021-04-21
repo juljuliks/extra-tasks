f3e5fc3d236e08c0c2fd0cb61cbdd02953bd3342
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-21 19:37:05 +0300

    extra-task

---
+++ trottlingDebounsing/script.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('input'),
+p = document.querySelector('p');
+
+
+const throttle = (fn, delay) => {
+    let last = 0;
+    return(...args) => {
+        const now = new Date().getTime();
+        if((now - last) < delay) {
+            return;
+        }
+        last = now;
+        return fn()
+    }
+}
+
+input.addEventListener('keydown', throttle(() => {
+    p.textContent = input.value;
+}, 300));
