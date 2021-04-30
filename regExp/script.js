dfe808d4744625a48ddee230b18834185c519a1f
Julia Glukhova <juliaglukhova666@gmail.com>
2021-04-30 22:41:24 +0300

    regexp

--- regExp/script.js
+++ regExp/script.js
@@ -19,12 +19,17 @@ const task3 = document.querySelector('#task2');
 const res3 = task3.innerHTML.match(/\".+\"/g);
 task3.innerHTML = task3.innerHTML.replace(/\".+\"/g, (str) => `<mark>${str}</mark>`);
 
-const task4 = document.querySelector('body');
-const res4 = task3.textContent.match(/[http]+:\/\/.+[a-z]/g);
-res4.forEach(el => {
-    console.log(el);
-    // task4.innerHTML = task4.innerHTML.replace(/[http]+:\/\/.+[a-z]/g, () => `<code>&lt a href="${el}" &gt</code>`)
-})
+let obj = {};
+document.body.innerText.replace(/https?:\/\/\S+([/$]|[\w+{2,}$])/gi, (match) => {
+  let key = match;
+  key = key.replace(/https?:\/\//gi, '')
+  key = key.replace(/\/\S+/gi, '')
+  obj[key] = match;
+});
+
+for (key in obj) {
+  document.body.innerHTML = document.body.innerHTML.replace(obj[key], `<a href="${obj[key]}">${key}</a>`);
+}
 
 const task5 = document.querySelector('body');
 const res5 = task5.innerHTML.match(/#[a-f0-9]{6}/gi);
